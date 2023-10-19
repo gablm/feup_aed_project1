d98440d2c012faf49e58fa3b1d95a2cf1639f482
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-10-19 20:23:12 +0100

    ui concept

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv) {
 	
-	std::cout << "Testing: " << argc << " args\nMy name is: " 
-		<< argv[0] << std::endl;
+	std::cout << u8"\033[2J\033[1;1H"; 
+	while (1)
+	{
+		std::cout << 
+"University of Porto - Faculty of Engineering\n\
+\n\
+Welcome to the Schedules service!\n\
+Please select an option [0-1]:\n\
+\n\
+  [0] Students\n\
+  [1] UCs\n\
+\n\
+Option: ";
+		std::string str;
+		std::cin >> str;
+		std::cout << u8"\033[2J\033[1;1H";
+	}
 	return 0;
 }
