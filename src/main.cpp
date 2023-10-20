90ef423d5fbbd25059e74cad5158dc3173b86562
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-10-20 12:23:29 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,31 @@
 
 int main(int argc, char **argv) {
 	
+	system("clear");
 	std::cout << u8"\033[2J\033[1;1H"; 
 	while (1)
 	{
-		std::cout << 
-"University of Porto - Faculty of Engineering\n\
-\n\
-Welcome to the Schedules service!\n\
-Please select an option [0-1]:\n\
-\n\
-  [0] Students\n\
-  [1] UCs\n\
-\n\
-Option: ";
+		std::cout << "University of Porto - Faculty of Engineering\n"
+				  << "\n"
+				  << "Welcome to the Schedules service!\n"
+				  << "Select an option [0-3]:\n"
+				  << "\n"
+				  << ">> List and search\n"
+				  << "\n"
+				  << "[0] Students\n"
+				  << "[1] UCs\n"
+				  << "[2] Classes\n"
+				  << "\n"
+				  << ">> Edit data\n"
+				  << "\n"
+				  << "[3] Change Student schedule\n"
+				  << "\n"
+				  << "Option: ";
 		std::string str;
 		std::cin >> str;
-		std::cout << u8"\033[2J\033[1;1H";
+		if (str == "q")
+			break;
+		system("clear");
 	}
 	return 0;
 }
