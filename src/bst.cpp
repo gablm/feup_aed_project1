733d0fa2abc0bd3c0422e636409020afe888db52
44784408+gablm@users.noreply.github.com <gabriell@DESKTOP-05MJE67>
2023-10-26 13:44:27 +0100

    a

--- src/bst.cpp
+++ src/bst.cpp
@@ -2,9 +2,24 @@
 
 BST::BST() : node(NULL) {};
 
+BSTnode *bst_new(int key, void *content)
+{
+	BSTnode *node = new BSTnode;
+	node->content = content;
+	node->key = key;
+	node->left = NULL;
+	node->right = NULL;
+}
+
 void BST::insert(int key, void *content)
 {
-
+	if (!node)
+	{
+		node = bst_new(key, content);
+		return ;
+	}
+	BSTnode *temp = node;
+	while (temp->left && temp->right)
 }
 
 BSTnode BST::search(int key)
