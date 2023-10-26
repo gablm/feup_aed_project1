1ba9701e20251d9b8699833a44f6565cb69cb6b9
44784408+gablm@users.noreply.github.com <gabriell@DESKTOP-05MJE67>
2023-10-26 15:17:26 +0100

    a

--- src/session.cpp
+++ src/session.cpp
@@ -1,48 +1,91 @@
 #include "session.h"
 #include <ostream>
 
+/**
+ * Default constructor
+ * Hour and Minute are set to 0
+*/
 Time::Time() : hour(0), minute(0) {}
 
+/**
+ * Constructor with two integers
+ * @param h Hour 
+ * @param n Minute
+*/
 Time::Time(int h, int m) : hour(h), minute(m) {}
 
+/**
+ * Complexity: O(1)
+ * @return Hours
+*/
 int Time::getHour() const {
 	return hour;
 }
 
+/**
+ * Complexity: O(1)
+ * @return Minutes
+*/
 int Time::getMinute() const {
 	return minute;
 }
 
+/**
+ * Streamlines printing the Time class
+*/
 std::ostream& operator<<(std::ostream& os, Time time)
 {
 	os << time.getHour() << ":" << time.getMinute();
 	return os;
 }
 
+/** Constructor for the session class
+ * @param name Session name (year, course_id, number)
+ * @param time Time the session will occur (h, m)
+ * @param type Type of session [T, TP, LP]
+*/
 Session::Session(SessionName name, Time time, std::string type)
 	: name(name), time(time), type(type) {}
 
+/**
+ * Complexity: O(1)
+ * @return Session name
+*/
+SessionName Session::getName() const {
+	return name;
+}
+
+/**
+ * Complexity: O(1)
+ * @return Session time
+*/
+Time Session::getTime() const {
+	return time;
+}
+
+/**
+ * Complexity: O(1)
+ * @return Session Type
+*/
+std::string Session::getType() const {
+	return type;
+}
+
+/**
+ * Complexity: O(1)
+ * @param student Pointer to a student
+*/
 void Session::addStudent(Student *student)
 {
 	studentList.push_back(student);
 }
 
-SessionName Session::getName() const {
-	return name;
-}
-
-Time Session::getTime() const {
-	return time;
-}
-
-std::string Session::getType() const {
-	return type;
-}
-
+/**
+ * Overload of the << operator for the SessionName class
+ * Streamlines the process of printing the Name for the session
+*/
 std::ostream& operator<<(std::ostream& os, SessionName name)
 {
-	if (name.year > 0)
-		os << name.year;
-	os << name.course_id << name.number;
+	os << name.year << name.course_id << name.number;
 	return os;
 }
