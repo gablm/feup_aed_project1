71dc69bd1c381bdefce334706fc61b20c56ed347
diogocp27 <diogocp27@hotmail.com>
2023-10-27 19:08:02 +0100

    partially implemented load_data

--- src/manager.cpp
+++ src/manager.cpp
@@ -1,19 +1,45 @@
 #include "manager.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 
 Manager::Manager() : students(BST()), ucMap() {}
 
 void Manager::load_data() {
 
-    string line;
+    string line, sessionName, UCName, sessionDay, sessionType, sessionTimestr, sessionDurationstr;
+    double sessionTime, sessionDuration;
 
     ifstream file;
     file.open("data/classes.csv");
 
+    getline(file,line); //removes the first line, which has no usable values
+
     while(!file.eof()){
 		getline(file,line);
-        cout << line << "\n";
+        istringstream ss(line);
 
+        // inserts the contents of the line into the apropriate variables, for the non-string variables an extra line was needed
+
+        std::getline(ss, sessionName, ',');
+        std::getline(ss, UCName, ',');
+        std::getline(ss, sessionDay, ',');
+        std::getline(ss, sessionTimestr, ',');
+        sessionTime = stof(sessionTimestr);
+        std::getline(ss, sessionDurationstr, ',');
+        sessionDuration = stof(sessionDurationstr);
+        std::getline(ss, sessionType, ',');
+
+        Session temp_session(sessionName, sessionDay, sessionTime, sessionDuration, sessionType);
+
+        cout << UCName << "\n";
+
+
+
+        //std::cout << sessionName << " " << UCName << " " << sessionDay << " " << sessionTime << " " << sessionDuration << " " << sessionType << '\n';
         
     }
+    file.close();
 }
--- src/manager.h
+++ src/manager.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <map>
+
 #include "bst.h"
 #include "student.h"
 #include "uc.h"
@@ -11,8 +12,10 @@
 class Manager {
 	private:
 		BST students;
+		static const int sessionCap = 25;
 		std::map<std::string, UC> ucMap;
 	public:
+		Manager();
 		void load_data();
 };
 
